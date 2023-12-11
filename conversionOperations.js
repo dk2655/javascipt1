5ad8bf16e3a85b89cfef22491c2e4fe6d07bbe29
Divyanshu Khajuria <105373674+dk2655@users.noreply.github.com>
2023-12-11 07:01:22 +0000

    //conversionOperations

--- conversionOperations.js
+++ conversionOperations.js
@@ -5,3 +5,19 @@ console.log(typeof(score))
 let valueinnumber = Number(score)
 console.log(typeof valueinnumber)
 console.log(valueinnumber);
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "dk"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+
+let randomnumber = 33
+
+let stringNumber = String(randomnumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
