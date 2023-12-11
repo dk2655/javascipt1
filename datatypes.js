a84493a6fc708bd7453489d9f5e584bba950ea65
Divyanshu Khajuria <105373674+dk2655@users.noreply.github.com>
2023-12-11 06:51:51 +0000

    Basuc DataTypes in Javascript

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert ("hello") ; this functiopn works in browser but here we are using node js so wait for some time and wee'll learn how to do this in JS
+
+// DATA TYPES
+
+let name = "DK"  
+let age = 20  
+let isLoggedIn = false
+
+/*string""
+  bigint (2 to the power 53; majorly used in stock markets.)
+  boolean => true/false
+  null => standalone value
+  undefined => abhi value define nhi ki hai bas variable delare kiya hai
+  symbol => unique
+  
+  object
+
+
+*/
+
+
+console.log(typeof "dk");
+console.log(typeof null);
+
