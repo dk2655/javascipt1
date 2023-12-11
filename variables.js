17b14ba4c1d928208f5370157efece9c5ba4fdec
Divyanshu Khajuria <105373674+dk2655@users.noreply.github.com>
2023-12-11 06:20:30 +0000

    `

---
+++ variables.js
@@ -0,0 +1,10 @@
+const account_id = 2677
+let accountemail = "dk@gmail.com"
+var accountpass = "26112003"
+accountCity = "jammu"
+
+// accountid = 2  ; cannot do this 
+
+console.log(account_id);
+
+console.table([account_id,accountemail,accountpass,accountCity])
