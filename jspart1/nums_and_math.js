e4f2bbeb6ef8e89ea4e8c5a6f2a293b5ae056364
Divyanshu Khajuria <105373674+dk2655@users.noreply.github.com>
2023-12-12 17:23:39 +0000

    newfile

---
+++ jspart1/nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(10));
+
+const newnum = 123.4566
+
+console.log(newnum.toPrecision(2));
+
+const othernum = 1000000
+console.log(othernum.toLocaleString()); // for intrenational number system
+
+console.log(othernum.toLocaleString('en-IN')); // for Indian number system
+
+
+
+//++++++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+//to experiece it type all this commands onm browser by inspecting.
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.66));  
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,5,6));
+console.log(Math.max(4,5,6,7));
+
+
+
+
+
+
+
+
+
