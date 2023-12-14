6ebfb38965a74217e9248064be4b8acd191eb070
Divyanshu Khajuria <105373674+dk2655@users.noreply.github.com>
2023-12-14 18:23:01 +0000

    completed part one basics

---
+++ iterations/filterandreduce.js
@@ -0,0 +1,62 @@
+  const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+  const newnums = mynums.filter(  (num) => {
+     return num > 4            //if u are using curly brackets then you have to return the condition
+  })
+
+  // console.log(newnums);
+  
+  
+  const newNums = mynums.filter(  (Num) => Num > 4)  // but without curlybrackets you dont need return statement
+
+  console.log(newNums);
+
+
+  //maps
+  const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNuums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNuums);
+
+
+//reduce function
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
